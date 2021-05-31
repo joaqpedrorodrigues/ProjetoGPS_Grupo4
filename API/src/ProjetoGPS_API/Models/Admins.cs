598224d3dca7e0c8df3c5c6666c2304bb8928502
Milkenm <miguel.campos.johnny@gmail.com>
2021-05-31 15:56:20 +0100

    Parece que o coiso da API tem de ser num repositório à parte

--- API/src/ProjetoGPS_API/Models/Admins.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ProjetoGPS_API.Models
-{
-	public class Admins
-	{
-		public long ID { get; set; }
-		public string Username { get; set; }
-		public string Password { get; set; }
-	}
-}
--- API/src/ProjetoGPS_API/Models/Placeholders.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ProjetoGPS_API.Models
-{
-	public class Placeholders
-	{
-		public long ID { get; set; }
-		public string Placeholder { get; set; }
-		public string Value { get; set; }
-	}
-}
